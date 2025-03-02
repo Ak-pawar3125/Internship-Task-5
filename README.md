1c9680f0ca8e094c2b81e461990601657d40a208
Ak-pawar3125 <akankshap25705@gmail.com>
2025-03-02 18:13:02 +0530

    add files to remote

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Internship-Task-5
+
+This project is a dynamic login form created using JavaScript, HTML, and CSS. It includes:<br>
+✅ Real-time email and password validation<br>
+✅ Dynamic error messages without alerts<br>
+✅ Toggle password visibility using an eye icon<br>
+✅ User-friendly UI with interactive elements<br>
+
+# Features
+🔹 Form Validation<br>
+
+Email Validation:<br>
+
+No uppercase letters allowed<br>
+Must contain '@' symbol<br>
+Displays error messages below the input<br>
+
+Password Validation:<br>
+
+Must be at least 8 characters long<br>
+Displays error messages dynamically<br>
+
+🔹 Password Visibility Toggle<br>
+
+Clicking the eye icon switches between password and text input.<br>
+Icon changes dynamically between fa-eye and fa-eye-slash.<br>
+
+
+# Technologies Used
+
+HTML – For form structure<br>
+CSS – For styling and error indication<br>
+JavaScript – For dynamic content, form validation, and interactivity<br>
+
+# Demo link and Screenshot
+
+Demo link : <br>
+
+Screen shot : ![alt text](image.png)
---
+++ script.js
@@ -0,0 +1,124 @@
+const formContainer = document.querySelector(".form-container");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.createElement("form");
+    form.id = "login-form";
+
+    const heading = document.createElement("h1");
+    heading.textContent = "Log in";
+    heading.id = "logHeading";
+
+    const emailLabel = document.createElement("label");
+    emailLabel.textContent = "Email / username";
+    emailLabel.setAttribute("for", "email-input");
+
+    const emailInput = document.createElement("input");
+    emailInput.type = "email";
+    emailInput.id = "email-input";
+    emailInput.name ="email";
+    emailInput.placeholder = "Enter your username";
+    emailInput.required = "true";
+
+    const passdiv = document.createElement("div");
+    
+    passdiv.id = "pass-sec";
+
+    const passLabel = document.createElement("label");
+    passLabel.textContent = "Password";
+    passLabel.setAttribute("for", "pass-input");  
+
+    const passInput = document.createElement("input");
+    passInput.type = "password";
+    passInput.id = "pass-input";
+    passInput.name = "password";
+    passInput.placeholder = "Enter your password";
+    passInput.required = "true";
+
+    
+    const loginBtn = document.createElement("button");
+    loginBtn.textContent = "Log in";
+    loginBtn.classList.add("submit");
+
+    const forgotdiv = document.createElement("div");
+    forgotdiv.classList.add("forgot-div");
+
+    const forgotLink = document.createElement("a");
+    forgotLink.textContent = "Forgot Password?";
+    forgotLink.href = "#";
+
+    const signUp = document.createElement("div");
+    signUp.classList.add("signup-div");
+
+    const signUpPara = document.createElement("p");
+    signUpPara.textContent = "New to Blogger";
+
+    const signUpLink = document.createElement("a");
+    signUpLink.textContent = "Create account";
+    signUpLink.href = "#";
+
+    const eyeIcon = document.createElement("i");
+    eyeIcon.classList.add("fa-solid", "fa-eye");
+    eyeIcon.style.cursor = "pointer";
+
+    form.appendChild(heading);
+    form.appendChild(emailLabel);
+    form.appendChild(emailInput);
+    form.appendChild(passdiv);
+    form.appendChild(passInput);
+    form.appendChild(loginBtn);
+    form.appendChild(forgotdiv);
+    form.appendChild(signUp)
+
+    forgotdiv.appendChild(forgotLink);
+    signUp.appendChild(signUpPara);
+    signUp.appendChild(signUpLink);
+    passdiv.appendChild(passLabel);
+    passdiv.appendChild(eyeIcon);
+
+    formContainer.appendChild(form);
+
+    eyeIcon.addEventListener("click", function () {
+        if (passInput.type === "password") {
+            passInput.type = "text";
+            eyeIcon.classList.replace("fa-eye", "fa-eye-slash");
+        } else {
+            passInput.type = "password";
+            eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
+        }
+    });
+
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault();
+        let isValid = "true";
+
+        const loginEmail = emailInput.value.trim();
+        const loginPass = passInput.value.trim();
+
+        if(loginEmail === "" || loginPass === ""){
+            alert("Please fill in both fields");
+            return;
+        }
+
+        if(loginPass.length < 8) {
+            alert("Password must be contain at least 8 characters");
+            return;
+        }
+
+        if(/[A-Z]/.test(loginEmail)){
+            alert("Enter a valid email (No uppercase letters allowed)");
+            return;
+        }
+
+        if(!loginEmail.includes("@gmail.com")){
+            alert("Enter a valid email");
+            return;
+        }
+
+        if(isValid){
+            alert("Login Successfully to Blooger");
+            window.location.href = "index.html";
+        }
+
+    });
+});
+
